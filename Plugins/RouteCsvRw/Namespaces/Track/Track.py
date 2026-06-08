e302240565f845d69eeda36e4024920ece9c970a
navya099 <qltmxkr@gmail.com>
2026-06-08 10:03:14 +0900

    TrackCommand.Turn 추가

--- Plugins/RouteCsvRw/Namespaces/Track/Track.py
+++ Plugins/RouteCsvRw/Namespaces/Track/Track.py
@@ -290,7 +290,16 @@ class Parser7:
                 data.Blocks[block_index].CurrentTrackState.CurveCantTangent = 0.0
 
             case TrackCommand.Turn:
-                pass
+                data.TurnUsed = True
+                s = 0.0
+                if len(arguments) >= 1 and len(arguments[0]) > 0:
+                    success, s = NumberFormats.try_parse_double_vb6(arguments[0])
+                    if not success:
+                        logger.error(f'Ratio is invalid in {command} at line '
+                                     f'{expression.Line} , column {expression.Column}'
+                                     f' in file {expression.File}')
+                        s = 0.0
+                    data.Blocks[block_index].Turn = s
             case TrackCommand.Adhesion:
                 pass
             case TrackCommand.Brightness:

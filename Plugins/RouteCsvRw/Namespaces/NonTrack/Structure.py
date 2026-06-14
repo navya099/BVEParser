09019f8a23bc652c8b72f3a92af53a1abdd2f969
navya099 <130723074+navya099@users.noreply.github.com>
2026-06-14 09:56:54 +0900

    StructureCommand.pole에 누락된 preview_only조건 추가

--- Plugins/RouteCsvRw/Namespaces/NonTrack/Structure.py
+++ Plugins/RouteCsvRw/Namespaces/NonTrack/Structure.py
@@ -43,35 +43,37 @@ class Parser10:
                             else:
                                 self.railtypeCount += 1
             case StructureCommand.Pole:
-                if command_indices[0] < 0:
-                    logger.error(f'AdditionalRailsCovered is expected to be non-negative in {command} at line '
-                                 f'{expression.Line}, column {expression.Column} in file {expression.File}')
-
-                elif command_indices[1] < 0:
-                    logger.error(f'PoleStructureIndex is expected to be non-negative in {command} at line '
-                                 f'{expression.Line}, column {expression.Column} in file {expression.File}')
-                else:
-                    if len(arguments) < 1:
-                        logger.error(f'{command} is expected to have one argument at line '
+                if not preview_only:
+                    if command_indices[0] < 0:
+                        logger.error(f'AdditionalRailsCovered is expected to be non-negative in {command} at line '
                                      f'{expression.Line}, column {expression.Column} in file {expression.File}')
 
-                    elif Path.contains_invalid_chars(arguments[0]):
-                        logger.error(f'FileName {arguments[0]} contains illegal characters in {command} at line '
+                    elif command_indices[1] < 0:
+                        logger.error(f'PoleStructureIndex is expected to be non-negative in {command} at line '
                                      f'{expression.Line}, column {expression.Column} in file {expression.File}')
                     else:
-                        if command_indices[0] not in data.Structure.Poles:
-                            data.Structure.Poles.Add(command_indices[0], ObjectDictionary())
-                        f = arguments[0]
-                        success, f = self.locate_object(f, self.ObjectPath)
-                        if not success:
-                            logger.error(f'FileName {f} not found in{command} at line '
+                        if len(arguments) < 1:
+                            logger.error(f'{command} is expected to have one argument at line '
                                          f'{expression.Line}, column {expression.Column} in file {expression.File}')
-                            self.missingObjectCount += 1
+
+                        elif Path.contains_invalid_chars(arguments[0]):
+                            logger.error(f'FileName {arguments[0]} contains illegal characters in {command} at line '
+                                         f'{expression.Line}, column {expression.Column} in file {expression.File}')
                         else:
-                            #success, obj = self.Plugin.CurrentHost.LoadObject(f, encoding)
-                            obj = [f]
-                            overwrite_default = True if command_indices[1] >= 0 and command_indices[1] >= 3 else False
-                            data.Structure.Poles[command_indices[0]].Add(command_indices[1], obj, overwrite_default)
+                            if command_indices[0] not in data.Structure.Poles:
+                                data.Structure.Poles.Add(command_indices[0], ObjectDictionary())
+                            f = arguments[0]
+                            success, f = self.locate_object(f, self.ObjectPath)
+                            if not success:
+                                logger.error(f'FileName {f} not found in{command} at line '
+                                             f'{expression.Line}, column {expression.Column} in file {expression.File}')
+                                self.missingObjectCount += 1
+                            else:
+                                #success, obj = self.Plugin.CurrentHost.LoadObject(f, encoding)
+                                obj = [f]
+                                overwrite_default = True if command_indices[1] >= 0 and command_indices[1] >= 3 else False
+                                data.Structure.Poles[command_indices[0]].Add(command_indices[1], obj, overwrite_default)
+            
             case StructureCommand.DikeL | StructureCommand.DikeR | StructureCommand.WallL | StructureCommand.WallR:
                 if not preview_only:
                     if command in (StructureCommand.DikeL, StructureCommand.DikeR):
@@ -117,6 +119,7 @@ class Parser10:
                                         data.Structure.DikeL[command_indices[0]] = obj
                                     elif command == StructureCommand.DikeR:
                                         data.Structure.DikeR[command_indices[0]] = obj
+
 
             case StructureCommand.Object | StructureCommand.FreeObj:
                 if command == StructureCommand.Object:
